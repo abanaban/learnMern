cd486d45a0e62cd422249f8edf8456e46b38c9fb
abanaban <69779321+abanaban@users.noreply.github.com>
2024-07-28 14:45:06 +0200

    create sign in api route and JWT token

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+import {errorHandler} from '../utils/error.js'
 
 export const signup = async (req, res, next) =>{
     const {username, email, password} = req.body;
@@ -13,4 +16,30 @@ export const signup = async (req, res, next) =>{
     }
     
     
+}
+
+export const signin = async (req, res, next) =>{
+    const {email, password} = req.body;
+
+    try {
+        const validUser = await User.findOne({email: email})
+        if(!validUser) {
+            console.log('wrong email')
+            return next(errorHandler(404, 'User not found!'))
+        }
+        console.log(validUser.password)
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+        console.log(validUser.password)
+        if(!validPassword){
+            return next(errorHandler(401, 'Wrong credentials!'))
+        }
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
+        const {password: pass, ...rest} = validUser._doc;// for the document of the database
+        res.cookie('access_token', token, {httpOnly: true}).status(200).json(rest);
+        
+    } catch (error) {
+        next(error)
+    }
+
+
 }
