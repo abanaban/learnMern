17a715c86a0329bb0a633045c75304c1a7101268
abanaban <69779321+abanaban@users.noreply.github.com>
2024-07-30 03:42:43 +0200

    create user update api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,9 @@
 import express, { json } from "express";
-import  {test} from "../controllers/user.controller.js";
+import  {test, updateUser} from "../controllers/user.controller.js";
+import { verifyToken } from "../verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
-
+router.post('/update/:id',verifyToken, updateUser)
 export default router;
