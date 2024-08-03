309974bcebba5262eb4bed013f9df278dbc6d590
abanaban <69779321+abanaban@users.noreply.github.com>
2024-08-03 18:50:46 +0200

    Add Show More button functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -6,7 +6,7 @@ export default function Search() {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
   const [listings, setListings] = useState([]);
-
+  const [showMore, setShowMore] = useState(false)
   const [sidebarData, setSidebarData] = useState({
     searchTerm: "",
     type: "all",
@@ -91,15 +91,35 @@ export default function Search() {
       });
     }
     const fetchListings = async () => {
+        setShowMore(false)
       setLoading(true);
       const searchQuery = urlParams.toString();
       const res = await fetch(`/api/listing/get?${searchQuery}`);
       const data = await res.json();
+      if(data.length > 8) {
+        setShowMore(true)
+      }else{
+        setShowMore(false)
+    }
       setListings(data);
       setLoading(false);
     };
     fetchListings();
   }, [location.search]);
+  const onShowmoreClick= async ()=> {
+    const numberOfListings = listings.length;
+    const startIndex = numberOfListings;
+    const urlParams = new URLSearchParams(location.search)
+    urlParams.set('startIndex', startIndex)
+    const searchQuery = urlParams.toString()
+    const res = await fetch(`/api/listing/get?${searchQuery}`);
+    const data = await res.json()
+    if(data.length < 9){
+        setShowMore(false)
+    }
+    setListings([...listings, ...data])
+
+  }
   return (
     <div className="flex flex-col md:flex-row">
       <div className="p-7 border-b-2 md:border-r-2 md:min-h-screen">
@@ -224,6 +244,15 @@ export default function Search() {
                     <ListingItem key={listing._id} listing={listing} />
 
                 ))}
+            {showMore && (
+                <button
+                onClick={()=>
+                    onShowmoreClick()}
+                className="text-green-700 hover:underline p-7 text-center w-full"
+                >
+                    Show More
+                </button>
+            )}
         </div>
       </div>
     </div>
