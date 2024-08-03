e96d85ec149f706fd0672e6afe81588db01fb77d
abanaban <69779321+abanaban@users.noreply.github.com>
2024-08-03 17:24:54 +0200

    create the listing item component and show listings

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -81,7 +81,7 @@ const Listing = () => {
                 <div className='flex gap-4'>
                     <p className='bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-lg'>{ listing.type === 'rent'? 'For Rent' : 'For Sale'}</p>
                     {listing.offer && (
-                        <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-lg'>${+listing.regularPrice - +listing.discountPrice}</p>
+                        <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-lg'>${+listing.regularPrice - +listing.discountPrice} Off</p>
                     )}
                 </div>
                 
