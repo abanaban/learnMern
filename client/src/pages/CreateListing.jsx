8ba8e1a3c3cd4d5543f786e47d90ccbe6cc372d3
abanaban <69779321+abanaban@users.noreply.github.com>
2024-08-04 00:41:43 +0200

    some changes in the uploading image listing location

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -62,7 +62,7 @@ const CreateListing = () => {
     return new Promise((resolve, reject) => {
       const storage = getStorage(app);
       const fileName = new Date().getTime() + file.name;
-      const storageRef = ref(storage, fileName);
+      const storageRef = ref(storage, `Listings/${fileName}`);
       const uploadTask = uploadBytesResumable(storageRef, file);
       uploadTask.on(
         "state_changed",
